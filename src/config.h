7018437e5471e578d20a08190a4fde74f8ac06f6
Ricardo Crudo <ricardo.crudo@gmail.com>
2017-02-24 10:03:24 -0300

    add config file

---
+++ src/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define CC_USE_EXTERNAL_CONFIG
+
+// maximum number of devices that can created
+#define CC_MAX_DEVICES      1
+// maximum number of actuators that can created per device
+#define CC_MAX_ACTUATORS    2
+// maximum number of assignments that can created per actuator
+#define CC_MAX_ASSIGNMENTS  1
+
+#endif

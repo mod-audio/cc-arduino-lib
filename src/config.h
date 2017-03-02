a196d166dc7d0cd86d320ae3d31cf849825d0456
Ricardo Crudo <ricardo.crudo@gmail.com>
2017-03-02 15:21:29 -0300

    add firmware version

--- src/config.h
+++ src/config.h
@@ -3,11 +3,16 @@
 
 #define CC_USE_EXTERNAL_CONFIG
 
-// maximum number of devices that can created
+// define firmware version
+#define CC_FIRMWARE_MAJOR   0
+#define CC_FIRMWARE_MINOR   0
+#define CC_FIRMWARE_MICRO   0
+
+// maximum number of devices that can be created
 #define CC_MAX_DEVICES      1
-// maximum number of actuators that can created per device
+// maximum number of actuators that can be created per device
 #define CC_MAX_ACTUATORS    2
-// maximum number of assignments that can created per actuator
+// maximum number of assignments that can be created per actuator
 #define CC_MAX_ASSIGNMENTS  1
 
 #endif

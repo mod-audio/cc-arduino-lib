9abd1ff9afdcfc49f063202e19dee307c1e33954
Ricardo Crudo <ricardo.crudo@gmail.com>
2017-03-31 17:16:16 -0300

    packing script update version file

--- pack-library.sh
+++ pack-library.sh
@@ -5,6 +5,14 @@ OUT_DIR=.out
 rm -rf ${OUT_DIR}
 mkdir -p ${OUT_DIR}
 
+# get version
+MAJOR=$(grep CC_FIRMWARE_MAJOR src/config.h | awk '{print $3}')
+MINOR=$(grep CC_FIRMWARE_MINOR src/config.h | awk '{print $3}')
+MICRO=$(grep CC_FIRMWARE_MICRO src/config.h | awk '{print $3}')
+
+# update version file
+echo "v${MAJOR}.${MINOR}.${MICRO}" > VERSION
+
 cp VERSION ${OUT_DIR}
 cp src/*.h src/*.cpp ${OUT_DIR}
 cp src/cc-slave/src/* ${OUT_DIR}

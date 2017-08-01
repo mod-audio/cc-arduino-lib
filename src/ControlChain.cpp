8704c3373b1bc5e1708e1aa56dbf9827ad2bfb42
Ricardo Crudo <ricardo.crudo@gmail.com>
2017-08-01 11:36:14 -0300

    add cast to void pointers (fix Due building)

--- src/ControlChain.cpp
+++ src/ControlChain.cpp
@@ -40,11 +40,11 @@ void ControlChain::addActuator(cc_device_t *device, cc_actuator_t *actuator) {
 
 void ControlChain::setEventCallback(int event_id, void (*function_cb)(void *arg)) {
     if (event_id == CC_EV_ASSIGNMENT) {
-        assignment_cb = function_cb;
+        assignment_cb = (void (*)(cc_assignment_t*)) function_cb;
     } else if (event_id == CC_EV_UNASSIGNMENT) {
-        unassignment_cb = function_cb;
+        unassignment_cb = (void (*)(int)) function_cb;
     } else if (event_id == CC_EV_UPDATE) {
-        update_cb = function_cb;
+        update_cb = (void (*)(cc_assignment_t*)) function_cb;
     }
 }
 
@@ -66,21 +66,21 @@ void ControlChain::responseCB(void *arg) {
 }
 
 void ControlChain::eventsCB(void *arg) {
-    cc_event_t *event = arg;
+    cc_event_t *event = (cc_event_t *) arg;
 
     if (event->id == CC_EV_ASSIGNMENT) {
-        cc_assignment_t *assignment = event->data;
+        cc_assignment_t *assignment = (cc_assignment_t *) event->data;
 
         if (assignment_cb)
             assignment_cb(assignment);
     } else if (event->id == CC_EV_UNASSIGNMENT) {
-        int *act_id = event->data;
+        int *act_id = (int *) event->data;
         int actuator_id = *act_id;
 
         if (unassignment_cb)
             unassignment_cb(actuator_id);
     } else if (event->id == CC_EV_UPDATE) {
-        cc_assignment_t *assignment = event->data;
+        cc_assignment_t *assignment = (cc_assignment_t *) event->data;
 
         if (update_cb)
             update_cb(assignment);
